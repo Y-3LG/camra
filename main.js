215dfb7334cc2437eb19355305c9c26e2bcc02b2
Yanitho's <77212067+Y-3LG@users.noreply.github.com>
2024-10-18 11:44:12 -0400

    Alternar camara

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
-navigator.mediaDevices.getUserMedia({video: true}).then((stream)=>{
-    console.log(stream)
+document.getElementById('toggleCameraButton').addEventListener('click', async () => {
+    const video = document.getElementById('video');
+    const devices = await navigator.mediaDevices.enumerateDevices();
 
-    let video = document.getElementById('video')
+    let deviceId;
 
-    video.srcObject = stream
+    devices.forEach(device => {
+        if (device.kind === 'videoinput' && device.label.toLowerCase().includes('back')) {
+            deviceId = device.deviceId;
+        } else if (device.kind === 'videoinput' && device.label.toLowerCase().includes('front')) {
+            deviceId = device.deviceId;
+        }
+    });
 
-    video.onloadedmetadata = (ev) => video.play()
+    const constraints = {
+        video: { deviceId: { exact: deviceId } }
+    };
 
-}).catch((err)=>console.log(err))
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        video.srcObject = stream;
+        video.play();
+    } catch (error) {
+        console.error('Error al cambiar de cámara: ', error);
+    }
+});
+
+navigator.mediaDevices.getUserMedia({ video: true })
+    .then(stream => {
+        let video = document.getElementById('video');
+        video.srcObject = stream;
+        video.onloadedmetadata = (ev) => video.play();
+    })
+    .catch(error => console.error('Error al acceder a la cámara: ', error));
